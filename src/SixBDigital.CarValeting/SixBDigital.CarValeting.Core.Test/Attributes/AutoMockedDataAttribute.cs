89f4c38403e7cf7d18af95676262d45d6087196f
Fábio Soares <drawtext@gmail.com>
2021-04-15 16:06:26 +0200

    Add unit tests for create booking (14h45-15h05)

---
+++ src/SixBDigital.CarValeting/SixBDigital.CarValeting.Core.Test/Attributes/AutoMockedDataAttribute.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Kernel;
+using AutoFixture.Xunit2;
+using Xunit;
+
+namespace SixBDigital.CarValeting.Core.Test.Attributes
+{
+    public class AutoMockedDataAttribute : AutoDataAttribute
+    {
+        public AutoMockedDataAttribute()
+            : base(CustomizeFixture)
+        { }
+
+        private static IFixture CustomizeFixture()
+        {
+            var fixture = new Fixture();
+            fixture.Customize(new AutoNSubstituteCustomization { ConfigureMembers = true });
+            
+            fixture.Behaviors
+                .OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior(2));
+
+            return fixture;
+        }
+    }
+
+    public class InlineAutoMockedDataAttribute : CompositeDataAttribute
+    {
+        public InlineAutoMockedDataAttribute()
+            : this(new AutoMockedDataAttribute())
+        { }
+
+        public InlineAutoMockedDataAttribute(params object[] values)
+            : this(new AutoMockedDataAttribute(), values)
+        { }
+
+        public InlineAutoMockedDataAttribute(AutoMockedDataAttribute autoDataAttributeAttribute, params object[] values)
+            : base(new InlineDataAttribute(values), autoDataAttributeAttribute)
+        { }
+    }
+
+    public class GenerationDepthBehavior : ISpecimenBuilderTransformation
+    {
+        public int Depth { get; }
+
+        public GenerationDepthBehavior(int depth)
+        {
+            Depth = depth;
+        }
+
+        public ISpecimenBuilderNode Transform(ISpecimenBuilder builder)
+        {
+            return new RecursionGuard(builder, new OmitOnRecursionHandler(), new IsSeededRequestComparer(), Depth);
+        }
+
+        private class IsSeededRequestComparer : IEqualityComparer
+        {
+            bool IEqualityComparer.Equals(object x, object y)
+            {
+                return x is SeededRequest && y is SeededRequest;
+            }
+
+            int IEqualityComparer.GetHashCode(object obj)
+            {
+                return obj is SeededRequest ? 0 : EqualityComparer<object>.Default.GetHashCode(obj);
+            }
+        }
+    }
+}
---
+++ src/SixBDigital.CarValeting/SixBDigital.CarValeting.Core.Test/Services/BookingServiceTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using FluentValidation;
+using NSubstitute;
+using SixBDigital.CarValeting.Core.DTOs;
+using SixBDigital.CarValeting.Core.Entities;
+using SixBDigital.CarValeting.Core.Interfaces;
+using SixBDigital.CarValeting.Core.Services;
+using SixBDigital.CarValeting.Core.Test.Attributes;
+using Xunit;
+
+namespace SixBDigital.CarValeting.Core.Test.Services
+{
+    public class BookingServiceTest
+    {
+        [Theory]
+        [AutoMockedData]
+        public void CreateBookingAsync_ShouldThrowArgumentNullException_WhenArgumentIsNull(
+            BookingService bookingService)
+        {
+            // ARRANGE
+            CreateBookingDTO createBooking = null;
+
+            // ACT
+
+            Func<Task<int>> result = async () => await bookingService.CreateBookingAsync(createBooking);
+
+            // ASSERT
+
+            result.Should().Throw<ArgumentNullException>()
+                .WithMessage("*createBooking*");
+        }
+
+        [Theory]
+        [AutoMockedData]
+        public void CreateBookingAsync_ShouldValidationException_WhenNameIsEmpty(
+            CreateBookingDTO createBooking,
+            BookingService bookingService)
+        {
+            // ARRANGE
+            createBooking.Name = "";
+            createBooking.EmailAddress = "ssahhd@gmail.com";
+
+            // ACT
+
+            Func<Task<int>> result = async () => await bookingService.CreateBookingAsync(createBooking);
+
+            // ASSERT
+
+            result.Should().Throw<ValidationException>()
+                .WithMessage("Booking is invalid");
+        }
+
+        [Theory]
+        [AutoMockedData]
+        public void CreateBookingAsync_ShouldValidationException_WhenContactNumberIsEmpty(
+            CreateBookingDTO createBooking,
+            BookingService bookingService)
+        {
+            // ARRANGE
+            createBooking.Name = "nbnb";
+            createBooking.EmailAddress = "ssahhd@gmail.com";
+            createBooking.ContactNumber = "";
+
+            // ACT
+
+            Func<Task<int>> result = async () => await bookingService.CreateBookingAsync(createBooking);
+
+            // ASSERT
+
+            result.Should().Throw<ValidationException>()
+                .WithMessage("Booking is invalid");
+        }
+
+        [Theory]
+        [AutoMockedData]
+        public void CreateBookingAsync_ShouldValidationException_WhenEmailAddressIsEmpty(
+            CreateBookingDTO createBooking,
+            BookingService bookingService)
+        {
+            // ARRANGE
+            createBooking.Name = "nbnb";
+            createBooking.EmailAddress = "";
+
+            // ACT
+
+            Func<Task<int>> result = async () => await bookingService.CreateBookingAsync(createBooking);
+
+            // ASSERT
+
+            result.Should().Throw<ValidationException>()
+                .WithMessage("Booking is invalid");
+        }
+
+
+        [Theory]
+        [AutoMockedData]
+        public void CreateBookingAsync_ShouldValidationException_WhenEmailAddressIsInvalid(
+                CreateBookingDTO createBooking,
+                BookingService bookingService)
+        {
+            // ARRANGE
+            createBooking.Name = "nbnb";
+            
+            // ACT
+            
+            Func<Task<int>> result = async () => await bookingService.CreateBookingAsync(createBooking);
+            
+            // ASSERT
+            
+            result.Should().Throw<ValidationException>()
+                    .WithMessage("Booking is invalid");
+        }
+
+        [Theory]
+        [AutoMockedData]
+        public async Task CreateBookingAsync_ShouldAddBooking_WhenThereIsNoErrors(
+            CreateBookingDTO createBooking,
+            [Frozen] IRepository<Booking> bookingRepository,
+            BookingService bookingService)
+        {
+            // ARRANGE
+            createBooking.EmailAddress = "ssahhd@gmail.com";
+            bookingRepository.CreateAsync(Arg.Any<Booking>()).Returns(1);
+
+            // ACT
+
+            var result = await bookingService.CreateBookingAsync(createBooking);
+
+            // ASSERT
+
+            result.Should().Be(1);
+        }
+
+    }
+    
+}
